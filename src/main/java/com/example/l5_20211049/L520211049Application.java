9133ab5f2d3287ba67017f88b6b789dbcc438f4b
AlexiaPT <153009848+Alexiaaaaaaasdde@users.noreply.github.com>
2025-05-16 20:30:37 -0500

    lab5

---
+++ src/main/java/com/example/l5_20211049/L520211049Application.java
@@ -0,0 +1,13 @@
+package com.example.l5_20211049;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L520211049Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L520211049Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/l5_20211049/L520211049ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.l5_20211049;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L520211049ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
